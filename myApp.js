75793ee4e745259e18c0d7cab1d888f2753f4fd5
dietcokechan <dietcokechan@gmail.com>
2023-09-15 20:04:20 +0300

    diable client side caching

--- myApp.js
+++ myApp.js
@@ -11,6 +11,7 @@ app.use(helmet.ieNoOpen());
 const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
 
 module.exports = app;
 const api = require('./server.js');
