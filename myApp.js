01c596adf5734a54b4729f71f6eb086c6b51d194
dietcokechan <dietcokechan@gmail.com>
2023-09-15 20:12:30 +0300

    set a content security policy

--- myApp.js
+++ myApp.js
@@ -12,6 +12,7 @@ const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({maxAge: ninetyDaysInSeconds, force: true}));
 app.use(helmet.dnsPrefetchControl());
 app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({directives: {deaultSrc: ["'self'"], scriptSrc: [('self'), 'trusted-cdn.com']}}));
 
 module.exports = app;
 const api = require('./server.js');
