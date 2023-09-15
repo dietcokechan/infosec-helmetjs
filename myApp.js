db60ddcdd42cbb6a5ed7b5d2ac46f6fbc0520543
dietcokechan <dietcokechan@gmail.com>
2023-09-15 19:52:23 +0300

    fix

--- myApp.js
+++ myApp.js
@@ -1,56 +1,18 @@
 const express = require('express');
-const helmet = require('helmet');
 const app = express();
-
-var ninetyDaysInSeconds = 7776000;
+const helmet = require('helmet');
 
 app.use(helmet.hidePoweredBy());
 app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+
+const ninetyDaysInSeconds = 90*24*60*60;
 app.use(helmet.hsts({
   maxAge: ninetyDaysInSeconds,
   force: true
 }));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = app;
 const api = require('./server.js');
